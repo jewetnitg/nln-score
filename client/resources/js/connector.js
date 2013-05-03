a020e7836f2c58c3d881e390571e9c9ee811c974
Jegor van Opdorp <jegor@edia.nl>
2013-05-03 14:41:38 +0200

    added xml files

--- client/resources/js/connector.js
+++ client/resources/js/connector.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://54.214.3.38:3000');//configure this to point to the nodejs server
+var socket = io.connect('http://localhost:3000');//configure this to point to the nodejs server
 
 socket.on('connected', function (data) {
 	console.log("connected",data);
