9c60e025d84e057bb733b125d04969f68453760c
Jegor van Opdorp <supradeus@hotmail.com>
2013-11-01 12:14:49 +0100

    Update notesDisplay.js

--- client/resources/js/notesDisplay.js
+++ client/resources/js/notesDisplay.js
@@ -52,7 +52,8 @@ function changeScoreImg(element,newFragmentPath){
 	console.log("typeof(document.images[newFragmentPath]): " + typeof(document.images[newFragmentPath]));
 	console.log('document.images[newFragmentPath]) != "undefined" : ' + document.images[newFragmentPath]) != "undefined");
 	console.log('document.images[newFragmentPath]) != undefined : ' + document.images[newFragmentPath]) != undefined);
-	if(typeof(document.images[newFragmentPath]) != "undefined" && 
+	console.log('document.images[newFragmentPath]) != undefined'+typeof document.images[newFragmentPath] != "undefined");
+	if(typeof document.images[newFragmentPath] != "undefined" && 
 		($(".fragment div" )[0] == document.images[newFragmentPath][0] ||
 		$(".fragment div" )[1] == document.images[newFragmentPath][0])  ){
 		$(element).html(document.images[newFragmentPath].clone());
