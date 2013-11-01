49f895879f933debfe0213a3f252bcc4e2acf5dd
Jegor van Opdorp <supradeus@hotmail.com>
2013-11-01 11:47:28 +0100

    Update notesDisplay.js

--- client/resources/js/notesDisplay.js
+++ client/resources/js/notesDisplay.js
@@ -47,6 +47,8 @@ function changeScoreXml(element,scoreXmlPath){
 }
 
 function changeScoreImg(element,newFragmentPath){
+	console.log("document.images[newFragmentPath]", document.images[newFragmentPath]);
+	console.log('$(".fragment div" )',$(".fragment div" ));
 	if(document.images[newFragmentPath] != undefined && 
 		($(".fragment div" )[0] == document.images[newFragmentPath][0] ||
 		$(".fragment div" )[1] == document.images[newFragmentPath][0])  ){
